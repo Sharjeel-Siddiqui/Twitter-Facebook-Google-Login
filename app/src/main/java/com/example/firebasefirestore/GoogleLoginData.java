d81f490d4c1fb746c7ecdb0261c5a0c90b875eef
sharjeel <sharjeelahmadsiddiqui@gmail.com>
2020-05-04 00:44:26 +0500

    first Commit when google API login works fine

---
+++ app/src/main/java/com/example/firebasefirestore/GoogleLoginData.java
@@ -0,0 +1,51 @@
+package com.example.firebasefirestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+public class GoogleLoginData extends AppCompatActivity {
+
+    ImageView iv;
+    TextView name, email, id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_login_data);
+
+        iv = findViewById(R.id.imageView);
+        name = findViewById(R.id.Name);
+        email = findViewById(R.id.Email);
+        id = findViewById(R.id.Id);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            System.out.println(personPhoto.toString());
+           /* Glide
+                    .with(this)
+                    .load(String.valueOf(personPhoto))
+                    .apply(new RequestOptions().override(600, 200))
+                    .into(iv);*/
+
+            name.setText(personName);
+            email.setText(personEmail);
+            id.setText(personId);
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/firebasefirestore/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.firebasefirestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    GoogleSignInClient mGoogleSignInClient;
+    Button login;
+    SignInButton signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken("877699020301-ubdgdt0v8t6uejvqpgg1rq80vo1l8slq.apps.googleusercontent.com")
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
+
+        login = findViewById(R.id.loginCustom);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.loginCustom:
+                 signIn();
+                break;
+            // ...
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 0) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            System.out.println("EMAIL " + account.getEmail());
+            System.out.println("DISPLAY NAME " + account.getDisplayName());
+            System.out.println("Photo URL " + account.getPhotoUrl());
+            System.out.println("ID TOKEN " + account.getIdToken());
+            System.out.println("ID " + account.getId());
+            System.out.println("ACCOUNT" + account.getAccount().name);
+            System.out.println("GIVEN NAME" + account.getGivenName());
+            // Signed in successfully, show authenticated UI.
+            startActivity(new Intent(this, GoogleLoginData.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            System.out.println(e.getMessage());
+        }
+    }
+}
